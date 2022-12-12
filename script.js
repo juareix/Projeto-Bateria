b02f84865af374b0e45964475f30093d4f641cd9
juareix <juarezmaciel45@gmail.com>
2022-12-12 11:43:06 -0400

    add coments

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
+//fazendo a captura da tecla acionada
 document.body.addEventListener('keyup', (event) => {
     playSound( event.code.toLowerCase() );
   });
-  
+
+//fazendo a seleção do botão input 
   document.querySelector('.composer button').addEventListener('click', () => {
     let song = document.querySelector('#input').value;
     if(song !== '') {
       let songArray = song.split('');
-      playComposition(songArray);
+      playComposition(songArray);//manda o array pra ser executado
     }
   });
-  
+// function que vai receber a tecla acionada e relacionar ela ao audio correspondente
   function playSound(sound) {
-    let audioElement = document.querySelector(`#s_${sound}`);
-    let keyElement = document.querySelector(`div[data-key='${sound}']`);
+    let audioElement = document.querySelector(`#s_${sound}`);//selecionando o id do audio
+    let keyElement = document.querySelector(`div[data-key='${sound}']`);//selcionando a data-key correspondente pra incluir a classe active
   
-    if(audioElement) {
-      audioElement.currentTime=0;
-      audioElement.play();
+    if(audioElement) {//verifica se a tecla acionada é algumas das validas
+      audioElement.currentTime=0;// faz com o que o audio n precise terminar caso seja acionado antes disso
+      audioElement.play();//function q executabo audio
     }
   
-    if(keyElement) {
+    if(keyElement) {//verifica se a div existe e adiciona a active
       keyElement.classList.add('active');
-      setTimeout(() => {
+      setTimeout(() => {//espera um time e retira a active
         keyElement.classList.remove('active');
       }, 300);
     }
